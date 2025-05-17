fb5f91cd3e0a76965bb5040345df4437bcc85e32
ashaban <AShaban@innovationteam.com>
2025-05-17 14:47:54 +0300

    code clean up add ktlint

--- app/src/main/java/com/mendi/task/MainActivity.kt
+++ app/src/main/java/com/mendi/task/MainActivity.kt
@@ -14,34 +14,39 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.mendi.task.ui.theme.MendiTaskTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            MendiTaskTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+  override fun onCreate(
+    savedInstanceState: Bundle?,
+  ) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContent {
+      MendiTaskTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+          Greeting(
+            name = "Android",
+            modifier = Modifier.padding(innerPadding),
+          )
         }
+      }
     }
+  }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(
+  name: String,
+  modifier: Modifier = Modifier,
+) {
+  Text(
+    text = "Hello $name!",
+    modifier = modifier,
+  )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MendiTaskTheme {
-        Greeting("Android")
-    }
+  MendiTaskTheme {
+    Greeting("Android")
+  }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,38 @@
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.ktlint) apply false
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        debug.set(true)
+        verbose.set(true)
+        android.set(true)
+        outputToConsole.set(true)
+        outputColorName.set("RED")
+        ignoreFailures.set(true)
+        enableExperimentalRules.set(false)
+
+        reporters{
+            reporter(ReporterType.HTML)
+            reporter(ReporterType.CHECKSTYLE)
+        }
+
+        filter {
+            exclude("**/generated/**")
+            include("**/kotlin/**")
+        }
+
+        dependencies {
+
+        }
+
+    }
 }
